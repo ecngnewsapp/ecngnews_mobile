9c485f690d32a05ecab93001c512c1029399ba30
abdulbasitsaid <mrspeed518@gmail.com>
2020-05-26 11:49:45 +0100

    fetch news edited

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -32,5 +32,50 @@ class NewsService {
     //query latast news
     Query newsPostQuery =
         _newsCollectionReference.orderBy('date').limit(pageLimit);
+    // checking its not the last document
+    if (_lastDocument != null) {
+      newsPostQuery.startAfterDocument(_lastDocument);
+    }
+    // checking if we have no more news items
+
+    if (!_hasMoreNews) return;
+
+    //setting current request index
+    int currentRequestIndex = _allPagedResult.length;
+
+    newsPostQuery.snapshots().listen((newsSnapShots) {
+      if (newsSnapShots.documents.isNotEmpty) {
+        List<News> news = newsSnapShots.documents
+            .map((snapShots) => News.fromJson(snapShots.data))
+            .where((mappedItem) => mappedItem.title != null)
+            .toList();
+
+        //check it page exist
+        bool pageExist = currentRequestIndex < _allPagedResult.length;
+
+        if (pageExist) {
+          _allPagedResult[currentRequestIndex] = news;
+        }
+        // if it doesn't exist add
+        else {
+          _allPagedResult.add(news);
+        }
+
+        // concatinate news to be shown
+        List<News> allNews = _allPagedResult.fold<List<News>>(
+          List<News>(),
+          (initialValue, pageItems) => initialValue..addAll(pageItems),
+        );
+
+        // brotcast all news
+        newsController.add(allNews);
+
+        //saving last document only if user at current page
+        if (currentRequestIndex == _allPagedResult.length - 1) {
+          _lastDocument = newsSnapShots.documents.last;
+        }
+        _hasMoreNews = news.length == pageLimit;
+      }
+    });
   }
 }
