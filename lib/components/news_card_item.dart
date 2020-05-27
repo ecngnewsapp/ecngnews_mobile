b1a41e38e32571daee4fb8ea903713cb050f18fd
abdulbasitsaid <mrspeed518@gmail.com>
2020-05-27 17:49:07 +0100

    edited news_services

--- lib/components/news_card_item.dart
+++ lib/components/news_card_item.dart
@@ -3,6 +3,7 @@ import 'package:ecngnews/utils/ecng_assets.dart';
 import 'package:ecngnews/utils/ecng_theme.dart';
 import 'package:ecngnews/utils/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class NewsItemCard extends StatelessWidget {
   final News news;
@@ -15,7 +16,7 @@ class NewsItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: SizeConfig.heightMultiplier * 14,
+      height: SizeConfig.heightMultiplier * 24,
       margin: EdgeInsets.symmetric(
           vertical: SizeConfig.sizeMultiplier * .8,
           horizontal: SizeConfig.sizeMultiplier),
@@ -39,7 +40,7 @@ class NewsItemCard extends StatelessWidget {
             decoration: BoxDecoration(
               color: Theme.of(context).primaryColor,
               image: DecorationImage(
-                  fit: BoxFit.fill,
+                  fit: BoxFit.cover,
                   image: news.imageUrl != null && news.imageUrl.isNotEmpty
                       ? NetworkImage(news.imageUrl)
                       : AssetImage(EcngAssets.logo)),
@@ -47,19 +48,90 @@ class NewsItemCard extends StatelessWidget {
                 Radius.circular(SizeConfig.sizeMultiplier * 2),
               ),
             ),
-            width: SizeConfig.widthMultiplier * 40,
+            width: SizeConfig.widthMultiplier * 30,
           ),
           Container(
-            padding: EdgeInsets.all(SizeConfig.sizeMultiplier),
-            width: SizeConfig.widthMultiplier * 40,
-            child: Text(
-              news.title,
-              softWrap: true,
-              overflow: TextOverflow.fade,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyText1
-                  .copyWith(fontWeight: FontWeight.bold, fontSize: 14),
+            padding: EdgeInsets.all(SizeConfig.sizeMultiplier * 1.5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: SizeConfig.widthMultiplier * 60,
+                  height: SizeConfig.heightMultiplier * 10,
+                  child: Text(
+                    news.title,
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        .copyWith(fontWeight: FontWeight.bold, fontSize: 14),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: SizeConfig.sizeMultiplier * 1.2,
+                    vertical: SizeConfig.sizeMultiplier * .5,
+                  ),
+                  child: Text(
+                    '${news.category}',
+                    style: Theme.of(context).textTheme.bodyText1.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                  decoration: BoxDecoration(
+                      color: EcngColors.defaultIconColor,
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(SizeConfig.sizeMultiplier * 2),
+                        right: Radius.circular(SizeConfig.sizeMultiplier * 2),
+                      )),
+                ),
+                Divider(
+                  color: EcngColors.primaryColor,
+                ),
+                Container(
+                  width: SizeConfig.widthMultiplier * 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Icon(MaterialCommunityIcons.clock),
+                            Container(
+                              width: SizeConfig.widthMultiplier * 28,
+                              child: Text(
+                                '${news.date}',
+                                softWrap: false,
+                                overflow: TextOverflow.fade,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1
+                                    .copyWith(
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 12),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Icon(
+                              news.loves > 0
+                                  ? MaterialCommunityIcons.heart
+                                  : MaterialCommunityIcons.heart_outline,
+                            ),
+                            Text('${news.loves}'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -7,6 +7,8 @@ import 'package:injectable/injectable.dart';
 @lazySingleton
 class NewsService {
   var _newsCollectionReference = Firestore.instance.collection('contents');
+  // var _videoNewsReference =
+  //     Firestore.instance.collection('path').orderBy('').startAt('values');
   final StreamController<List<News>> _newsStreamController =
       StreamController<List<News>>.broadcast();
   Stream listenToNews() {
@@ -14,7 +16,22 @@ class NewsService {
       if (event.documents.isNotEmpty) {
         var news = event.documents
             .map((e) => News.fromJson(e.data))
-            .where((element) => element.title != null)
+            .where(
+              (element) => element.url != null,
+            )
+            .toList();
+        _newsStreamController.add(news);
+      }
+    });
+    return _newsStreamController.stream;
+  }
+
+  Stream getVideoNews() {
+    _newsCollectionReference.snapshots().listen((event) {
+      if (event.documents.isNotEmpty) {
+        var news = event.documents
+            .map((e) => News.fromJson(e.data))
+            .where((element) => element.url.isNotEmpty)
             .toList();
         _newsStreamController.add(news);
       }
