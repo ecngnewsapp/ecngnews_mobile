b52eb990400ecd39db99ac7af3462fa59c60f84f
abdulbasitsaid <mrspeed518@gmail.com>
2020-05-30 01:27:44 +0100

    converted watch to stateless

--- lib/views/watch/watch_view.dart
+++ lib/views/watch/watch_view.dart
@@ -1,15 +1,8 @@
-import 'package:ecngnews/smart_components/new_categories/news_categories.dart';
 import 'package:ecngnews/utils/ecng_theme.dart';
 import 'package:ecngnews/utils/size_config.dart';
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class WatchView extends StatefulWidget {
-  @override
-  _WatchViewState createState() => _WatchViewState();
-}
-
-class _WatchViewState extends State<WatchView> {
+class WatchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
