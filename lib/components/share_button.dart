adb590b9d0c8279deea1939db2d9b667a999af3e
abdulbasitsaid <mrspeed518@gmail.com>
2020-06-21 00:25:17 +0100

    share icons added

--- lib/components/share_button.dart
+++ lib/components/share_button.dart
@@ -1,6 +1,7 @@
 import 'package:ecngnews/utils/ecng_theme.dart';
 import 'package:ecngnews/utils/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class ShareButton extends StatefulWidget {
   const ShareButton({
@@ -21,37 +22,65 @@ class _ShareButtonState extends State<ShareButton> {
       child: Stack(
         children: [
           //click priventer
-          Positioned(
-            left: SizeConfig.widthMultiplier,
-            bottom: SizeConfig.heightMultiplier,
-            child: Container(
-              color: Colors.black.withOpacity(0.1),
-              height: isShareClicked ? SizeConfig.h : 0,
-              width: isShareClicked ? SizeConfig.w : 0,
-            ),
+          Container(
+            color: Colors.black.withOpacity(0.1),
+            height: isShareClicked ? SizeConfig.h : 0,
+            width: isShareClicked ? SizeConfig.w : 0,
           ),
           Positioned(
             left: SizeConfig.widthMultiplier,
             bottom: SizeConfig.heightMultiplier,
             child: Container(
-              width: SizeConfig.heightMultiplier * 15,
-              height: SizeConfig.heightMultiplier * 15,
+              width: SizeConfig.w,
+              height: SizeConfig.h,
               child: Stack(
                 children: [
+                  //whatsapp
                   AnimatedPositioned(
                     // top: 0,
-                    bottom: isShareClicked ? 100 : 0,
-                    duration: Duration(milliseconds: 200),
-                    child: CircleAvatar(),
+                    curve: Curves.easeInOut,
+                    bottom:
+                        // SizeConfig.heightMultiplier * 10,
+                        isShareClicked ? SizeConfig.heightMultiplier * 10 : 0,
+                    duration: Duration(milliseconds: 300),
+                    child: CircleAvatar(
+                      backgroundColor: Color(0xff25D366),
+                      child: Icon(
+                        FlutterIcons.whatsapp_faw,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                  Positioned(
-                    right: 0,
-                    child: CircleAvatar(),
+                  AnimatedPositioned(
+                    curve: Curves.easeInOut,
+                    bottom:
+                        isShareClicked ? SizeConfig.heightMultiplier * 8 : 0,
+                    duration: Duration(milliseconds: 400),
+                    left: isShareClicked ? SizeConfig.widthMultiplier * 14 : 0,
+                    child: CircleAvatar(
+                      backgroundColor: Color(0xff1DA1F2),
+                      child: Icon(
+                        FlutterIcons.twitter_ant,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                  Positioned(
+                  AnimatedPositioned(
+                    curve: Curves.easeInOut,
+                    duration: Duration(milliseconds: 500),
                     bottom: 0,
-                    right: 0,
-                    child: CircleAvatar(),
+                    left: isShareClicked ? SizeConfig.widthMultiplier * 16 : 0,
+                    child: AnimatedContainer(
+                        height: SizeConfig.sizeMultiplier * 5,
+                        width: SizeConfig.sizeMultiplier * 5,
+                        duration: Duration(milliseconds: 300),
+                        child: CircleAvatar(
+                          backgroundColor: Color(0xff4267B2),
+                          child: Icon(
+                            FlutterIcons.facebook_faw,
+                            color: Colors.white,
+                          ),
+                        )),
                   ),
                   //click share
                   Positioned(
